b59e87403b4d59ce5e3ed7be6a65ce60833ad230
leevivl <leevi.laune@metropolia.fi>
2025-11-17 15:22:23 +0200

    add authorization and error handler

--- src/api/controller/product-controller.js
+++ src/api/controller/product-controller.js
@@ -1,49 +1,70 @@
-import {product,option, allergen} from "../model/index.js";
+import { product, option, allergen } from "../model/index.js";
 
-const getAllProducts = async (req, res) => {
-    const products = await product.findAll({where:{default_product:true}, 
-        include:[
+const getAllProducts = async (req, res, next) => {
+    const products = await product.findAll({
+        where: { default_product: true },
+        include: [
             {
                 model: option,
-                as:'options',
-                attributes: ['option_id', 'name', 'description'],
-                through: { attributes: [] } 
+                as: "options",
+                attributes: ["option_id", "name", "description"],
+                through: { attributes: [] },
             },
             {
                 model: allergen,
-                as: 'allergens',
-                attributes: ['allergen_id', 'allergen_name', 'allergen_icon_url'],
-                through: {attributes: []}
+                as: "allergens",
+                attributes: [
+                    "allergen_id",
+                    "allergen_name",
+                    "allergen_icon_url",
+                ],
+                through: { attributes: [] },
             },
-    ]});
-    if(products.length > 0){
+        ],
+    });
+    if (products.length > 0) {
         res.json(products);
-    }else{
-        res.status(404).json({message: "Products is empty"})
+    } else {
+        const error = new Error("Products is empty");
+        error.status = 404;
+        console.log("getAllProducts", error);
+        next(error);
     }
 };
 
-const addProduct = async (req, res) => {
-    const {name, price, category, option_ids, description, image_url, allergen_ids, default_product} = req.body;
-    try{
+const addProduct = async (req, res, next) => {
+    const {
+        name,
+        price,
+        category,
+        option_ids,
+        description,
+        image_url,
+        allergen_ids,
+        default_product,
+    } = req.body;
+    try {
         const newProduct = await product.create({
             name: name,
             price: price,
             category: category,
             description: description,
             image_url: image_url,
-            default_product: default_product
+            default_product: default_product,
         });
-        if(option_ids && option_ids.length > 0){
+        if (option_ids && option_ids.length > 0) {
             await newProduct.setOptions(option_ids);
         }
-        if(allergen_ids && allergen_ids.length > 0){
+        if (allergen_ids && allergen_ids.length > 0) {
             await newProduct.setAllergens(allergen_ids);
         }
-        res.json({success: true, new: newProduct});
-    }catch(error){
-        res.status(400).json({message: error.message});
+        res.json({ success: true, new: newProduct });
+    } catch (err) {
+        const error = new Error("Could not create a product");
+        error.status = 400;
+        console.log("addProduct", error);
+        next(error);
     }
 };
 
-export {getAllProducts, addProduct};
+export { getAllProducts, addProduct };
--- src/index.js
+++ src/index.js
@@ -2,18 +2,30 @@ import express from "express";
 import sequelize from "./api/config/database.js";
 import { error } from "console";
 import productRouter from "./api/router/product-router.js";
+import authRouter from "./api/router/auth-router.js";
+import {
+    notFoundHandler,
+    errorHandler,
+} from "./api/middleware/error-handler.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.use("/api/v1/product",productRouter);
+app.use("/api/v1/product", productRouter);
+app.use("/api/v1/auth", authRouter);
 
-try{
-  await sequelize.authenticate();
-  await sequelize.sync({alter:true});
-  console.log("DB connection established");
-}catch (error){
-  console.error("DB connection failed", error);
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+try {
+    await sequelize.authenticate();
+    await sequelize.sync({ alter: true });
+    console.log("DB connection established");
+} catch (error) {
+    console.error("DB connection failed", error);
 }
 
 app.listen(3000);
