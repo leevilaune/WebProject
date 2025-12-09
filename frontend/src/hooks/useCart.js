1d1278e0537f7cd493c78d64889cd9300ac97f8e
sakari Honkavaara <Sakari.honkavaara@metropolia.fi>
2025-12-09 20:22:32 +0200

    localstorage to shoppingcart

--- frontend/src/hooks/useCart.js
+++ frontend/src/hooks/useCart.js
@@ -1,10 +1,22 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 
 export const useCart = () => {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(() => {
+    const stored = localStorage.getItem("cart");
+    if (stored) {
+        return JSON.parse(stored);
+        } else {
+        return [];
+        }
+  })
+  
   const [showCart, setShowCart] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState(null);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
 
   const openModal = (product) => {
     setSelectedProduct(product);
