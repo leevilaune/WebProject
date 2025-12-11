361dc34a6284af0b8f7f18f0510463c88493a426
Aleksipc <aleklap@metropolia.figit config --global user.emailgit config --global user.email aleklap@metropolia.fi>
2025-12-11 23:35:48 +0200

    allergens added

--- frontend/src/components/ProductItem.jsx
+++ frontend/src/components/ProductItem.jsx
@@ -1,30 +1,47 @@
-import getImageUrl from '../utils/getImageUrl';
+import getImageUrl from "../utils/getImageUrl";
 
 const ProductItem = ({ product, openModal }) => (
-  <li className="product-item">
-    <div className="product-info">
-      <h4 className="product-name">{product.name}</h4>
-      <p className="product-description">{product.description}</p>
-      <p className="product-price">{product.price}€</p>
-      {product.image_url && (
-        <img
-          className="product-image"
-          src={getImageUrl(product.image_url)}
-          alt={product.name}
-        />
-      )}
-    </div>
+    <li className="product-item">
+        <div className="product-info">
+            <h4 className="product-name">{product.name}</h4>
+            <p className="product-description">{product.description}</p>
+            <p className="product-price">{product.price}€</p>
+            {product.allergens?.length > 0 && (
+                <div
+                    className="product-allergens"
+                    style={{ paddingLeft: "10px" }}
+                >
+                    {product.allergens.map((a) => (
+                        <img
+                            key={a.allergen_id}
+                            src={getImageUrl(a.allergen_icon_url)}
+                            alt={a.allergen_name}
+                            title={a.allergen_name}
+                            style={{ width: 24, height: 24, marginRight: 4 }}
+                        />
+                    ))}
+                </div>
+            )}
 
-    <div className="product-actions">
-      <button
-        type="button"
-        className="add-to-cart-button"
-        onClick={() => openModal(product)}
-      >
-        Add to shopping cart
-      </button>
-    </div>
-  </li>
+            {product.image_url && (
+                <img
+                    className="product-image"
+                    src={getImageUrl(product.image_url)}
+                    alt={product.name}
+                />
+            )}
+        </div>
+
+        <div className="product-actions">
+            <button
+                type="button"
+                className="add-to-cart-button"
+                onClick={() => openModal(product)}
+            >
+                Add to shopping cart
+            </button>
+        </div>
+    </li>
 );
 
 export default ProductItem;
